f4aec027bfc49036a3f6991e725d55dd53612717
gemsioxa <deema.light@gmail.com>
2024-05-02 21:01:14 +0300

    style: home page layout

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import './style.css';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={'main'}>
+      <header className={'main__header'}>
+        Cloud Manager
+      </header>
+      <main className={'main__content'}>
+        <div className={'main__content__name'}>
+          Cloud Manager
+        </div>
+        <div className={'main__content__list'}>
+          <div>Free</div>
+          <div>Fast</div>
+          <div>Fantastic</div>
+        </div>
+      </main>
+      <footer className={'main__footer'}>
+        <div>
+          Author: Dmitriy Lebedev <br/>
+          Email: gemsioxabusiness@gmail.com
+        </div>
+        <div>
+          Cloud Manager Inc.
+        </div>
+      </footer>
+    </div>
   )
 }
